7ce1b1cd3e4b46fdee123499a7a9d60b8f70d93e
Valeria Meza <valeriameza173@gmail.com>
2021-09-22 22:36:46 -0500

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { React } from "react";
+import { Link } from "react-router-dom";
+
+export default () => {
+  return (
+    <div>
+      I'm some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
